fdb02e91244148580500d318e7d42cab3a0b53cf
Yolanda Jacobs <y.jacobs@hotmail.com>
2022-08-14 17:21:15 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # team-profile-generator
+
+Run 'node index.js' and follow the comand line prompts to generate a page with your team info.
+
+[Here is a link to the walkthrough video](https://drive.google.com/file/d/1LyI2ONvq5AGWiolt497aw0jJg_orRjjE/view)
--- index.js
+++ index.js
@@ -3,19 +3,19 @@ import { writeFile, copyFile } from './utils/generateHTML.js';
 import generateHTML from './src/page-template.js'
 
 new Input().initializeInput()
-    .then(teamData => {
-        return generateHTML(teamData);
-    })
-    .then(pageHTML => {
-        return writeFile(pageHTML);
-    })
-    .then(writeFileResponse => {
-        console.log(writeFileResponse);
-        return copyFile();
-    })
-    .then(copyFileResponse => {
-        console.log(copyFileResponse);
-    })
-    .catch(err => {
-        console.log(err);
-    });
+    // .then(teamData => {
+    //     return generateHTML(teamData);
+    // })
+    // .then(pageHTML => {
+    //     return writeFile(pageHTML);
+    // })
+    // .then(writeFileResponse => {
+    //     console.log(writeFileResponse);
+    //     return copyFile();
+    // })
+    // .then(copyFileResponse => {
+    //     console.log(copyFileResponse);
+    // })
+    // .catch(err => {
+    //     console.log(err);
+    // });
